6939f0631fdf814e110e184319ab9f91ef877b39
Tuan Anh <TranTuananh291299@gmail.com>
2022-07-13 14:25:13 +0700

    up date

--- app/src/main/java/com/tunanh/lfood/ativity/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/tunanh/lfood/ativity/adapter/CategoryAdapter.kt
@@ -1,36 +1,53 @@
 package com.tunanh.lfood.ativity.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.tunanh.lfood.R
 import com.tunanh.lfood.ativity.item.CategoryItem
+import com.tunanh.lfood.databinding.ItemCategoryBinding
+import com.tunanh.lfood.databinding.ItemRclvCategoryBinding
 
-class CategoryAdapter(var context: Context, var arrayList: ArrayList<CategoryItem>) :
-    BaseAdapter() {
-    override fun getCount(): Int {
+class CategoryAdapter(var context: Context, var arrayList: ArrayList<CategoryItem>,var type:Int) :RecyclerView.Adapter<CategoryAdapter.CategoryViewholder>() {
+    inner class CategoryViewholder(itemView: View):RecyclerView.ViewHolder(itemView){
+        var itemimg: ImageView
+        var itemText: TextView
+
+        init {
+            if (type==1){
+                itemimg = itemView.findViewById(R.id.img_category)
+                itemText = itemView.findViewById(R.id.text_category)
+            }else{
+                itemimg = itemView.findViewById(R.id.img_category_rcl)
+                itemText = itemView.findViewById(R.id.text_category_rclv)
+            }
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewholder {
+
+        if (type==1){
+        return CategoryViewholder(LayoutInflater.from(parent.context).inflate(R.layout.item_rclv_category,parent,false))
+        }else{
+            return CategoryViewholder(LayoutInflater.from(parent.context).inflate(R.layout.item_rclv_category,parent,false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewholder, position: Int) {
+        val currenItem = arrayList[position]
+        holder.itemimg.setImageBitmap(currenItem.icon)
+        holder.itemText.text = currenItem.name
+    }
+
+    override fun getItemCount(): Int {
         return arrayList.size
-    }
-
-    override fun getItem(position: Int): Any {
-        return arrayList.get(position)
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var view = View.inflate(context, R.layout.item_category, null)
-        var icon = view.findViewById<ImageView>(R.id.img_category)
-        var name = view.findViewById<TextView>(R.id.text_category)
-        var categoryItem = arrayList.get(position)
-        icon.setImageResource(categoryItem.icon)
-        name.text = categoryItem.name
-
-        return view
     }
 }
--- app/src/main/java/com/tunanh/lfood/ativity/item/CategoryItem.kt
+++ app/src/main/java/com/tunanh/lfood/ativity/item/CategoryItem.kt
@@ -1,10 +1,12 @@
 package com.tunanh.lfood.ativity.item
 
+import android.graphics.Bitmap
+
 class CategoryItem {
-    var icon: Int
+    var icon: Bitmap
     var name: String
 
-    public constructor(icon: Int, name: String) {
+    public constructor(icon: Bitmap, name: String) {
         this.icon = icon
         this.name = name
     }
